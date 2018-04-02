f8baab608c89853f0883abe9784a2ee2607eee6c
Baker Griffith <b.griffith@ufl.edu>
2018-04-02 00:52:55 -0400

    Initial commit

---
+++ Expression.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include "Expression.h"
+
+// Models a component of a mathematical expression.
+class Expression {
+
+public:
+    /* Calculates the value of the expression.*/
+    virtual int getValue() = 0;
+
+    /* Returns the requested operand for this expression* component.*/
+    virtual Expression* getLeftSide() = 0;
+
+    virtual Expression* getRightSide() = 0;
+
+    /* Returns the individual multiplicative factors of the* expression
+     * –only those of a positive power. (Thus,* belonging in the numerator if fractional.)*/
+    virtual vector <Expression*> getNumeratorFactors() = 0;
+
+    /* Returns the individual multiplicative factors of the* expression
+     * –only those of a negative power. (Thus,* belonging in the denominator if fractional.)*/
+    virtual vector <Expression*> getDenominatorFactors() = 0;
+
+    /* If the expression is the result of addition or * subtraction, returns the individual terms.*/
+    virtual vector <Expression*> getAdditiveTerms() = 0;
+
+    /* Signals the expression to produce a simplified version* of itself put into lowest terms.*/
+    virtual Expression* simplify() = 0;
+};
+
+int main() {
+
+}
